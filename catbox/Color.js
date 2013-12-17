dbccd145c134bffbea3397dc4a51f93d09663ae4
Velaxtor <velaxtor@gmail.com>
2013-12-17 16:31:37 +0100

    Add color presets

--- catbox/Color.js
+++ catbox/Color.js
@@ -5,6 +5,14 @@ var Color = Class.extend({
     this.r = r;
     this.g = g;
     this.b = b;
-    this.a = a;
+    this.a = a || 255;
   },
 });
+
+
+
+Color.__defineGetter__('red', function() { return new Color(255, 0, 0); });
+Color.__defineGetter__('green', function() { return new Color(0, 255, 0); });
+Color.__defineGetter__('blue', function() { return new Color(0, 0, 255); });
+Color.__defineGetter__('black', function() { return new Color(0, 0, 0); });
+Color.__defineGetter__('white', function() { return new Color(255, 255, 255); });
