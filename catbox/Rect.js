bbde30c1a74e3103f6e39cf3e5af422025b22a84
Zach Ewert <zachanima@gmail.com>
2013-12-17 20:37:35 +0100

    Add whitespace to Rect class.

--- catbox/Rect.js
+++ catbox/Rect.js
@@ -5,10 +5,13 @@ var Rect = Class.extend({
     this.position = new Vector2(x, y);
     this.width = width;
     this.height = height;
+
     var _this = this;
+
     this.__defineGetter__('min', function() { 
       return _this.position; 
     });
+
     this.__defineGetter__('max', function() {
       return new Vector2(x + width, y + height);
     });
