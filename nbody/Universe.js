26a3f188d5aeba4150c430ebdc35e675a374edf9
Zach Ewert <zachanima@gmail.com>
2013-12-19 22:13:06 +0100

    Add NBody example.

---
+++ nbody/Universe.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var Universe = Component.extend({
+  bodies: [],
+
+
+
+  Awake: function() {
+    for (var i = 384; i--;) {
+      var body = new GameObject('Body', Body);
+      body.Add(Sprite).Load('res/box.png');
+      body.Add(Rigidbody).useGravity = false;
+      body.Add(BoxCollider);
+      body.collider.width = 21;
+      body.collider.height = 21;
+      body.transform.position = new Vector2(Math.random() * canvas.width, Math.random() * canvas.height);
+      body.rigidbody.velocity = new Vector2(canvas.width / 2- Math.random() * canvas.width, canvas.height / 2 - Math.random() * canvas.height).Mul(1);
+      this.bodies.push(body.Get(Body));
+    }
+  },
+
+
+
+  Update: function() {
+    for (var i = this.bodies.length; i--;) {
+      for (var j = this.bodies.length; j--;) {
+        if (i != j) {
+          this.bodies[i].Attract(this.bodies[j]);
+        }
+      }
+    }
+  },
+});
