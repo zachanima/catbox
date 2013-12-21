306e3377fa55c5a71d5b69e504c4bd70d9c69250
Velaxtor <velaxtor@gmail.com>
2013-12-21 06:57:43 +0100

    Fix max number of particles on screen

--- shmup/Star.js
+++ shmup/Star.js
@@ -15,7 +15,7 @@ var Star = Component.augment(function(base) {
 
 
   this.Start = function() {
-    this.particleSystem.maxParticles = 50000;
+    this.particleSystem.maxParticles = 5000;
     this.transform.rotation = Math.PI;
     for (var i = 0; i < 768; ++i) {
       this.transform.position.x = Math.ceil(Math.random() * 800) + 0.25;
