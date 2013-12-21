05fe8827b9ae01f110fbc27d62009fc0cb54a484
Zach Ewert <zachanima@gmail.com>
2013-12-21 07:23:08 +0100

    Refurbish boxes example

--- boxes/Platform.js
+++ boxes/Platform.js
@@ -1,18 +1,19 @@
 "use strict";
 
-var Platform = Component.extend({
-  Awake: function() {
-    this.Add(BoxCollider);
-    this.collider.width = 100;
-    this.collider.height = 10;
+var Platform = Component.augment(function(base) {
+  this.constructor = function() { base.constructor.call(this) };
+
+
+
+  this.Awake = function() {
     this.lifetime = 0;
-  },
+  };
 
 
 
-  Update: function() {
+  this.Update = function() {
     this.transform.position.x = 400;
     this.transform.position.y = 480 + 120 * Math.sin(this.lifetime);
     this.lifetime += Time.deltaTime;
-  },
+  };
 });
