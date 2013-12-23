2191692bfa2ee1a863832350068958239cccf1ef
Velaxtor <velaxtor@gmail.com>
2013-12-23 22:55:45 +0100

    Fix particles

--- catbox/Objects/Particle.js
+++ catbox/Objects/Particle.js
@@ -10,6 +10,10 @@ var Particle = Object.augment(function(base) {
 
 
   this.Update = function() {
+    this.position.x += this.velocity.x * Time.deltaTime;
+    this.position.y += this.velocity.y * Time.deltaTime;
+    this.velocity.x += Physics.gravity.x * this.particleSystem.gravityModifier;
+    this.velocity.y += Physics.gravity.y * this.particleSystem.gravityModifier;
     //Destroy after some time
     this.lifetime -= Time.deltaTime;
     if (this.lifetime <= 0) {
@@ -20,10 +24,6 @@ var Particle = Object.augment(function(base) {
 
 
   this.SimulatePhysics = function() {
-    this.position.x += this.velocity.x * Time.fixedDeltaTime;
-    this.position.y += this.velocity.y * Time.fixedDeltaTime;
-    this.velocity.x += Physics.gravity.x * this.particleSystem.gravityModifier;
-    this.velocity.y += Physics.gravity.y * this.particleSystem.gravityModifier;
   };
 
 
